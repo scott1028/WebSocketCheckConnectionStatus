d6a679839a5465aa704353ade3e09e9819d5ec17
scott <mic1028002@gmail.com>
2015-10-10 00:34:48 +0800

    update code

--- app.js
+++ app.js
@@ -2,34 +2,64 @@
 // Implment a webSocket server with connection status checker.
 
 
-var WebSocketServer = require('ws').Server, wss = new WebSocketServer({ port: 8090 });
+//
+var WebSocketServer = require('ws').Server,
+    wss = new WebSocketServer({
+        port: 8090
+    });
 
+
+//
 wss.on('connection', function connection(ws) {
 
-  //
-  console.log('connected!');
+    //
+    console.log('connected!');
 
 
-  //
-  ws.on('message', function incoming(message) {
-    console.log('received: %s', message);
-  });
+    //
+    ws.on('message', function incoming(message) {
+        console.log('received: %s', message);
+    });
 
 
-  // Implement connecton checker
-  ws.on('pong', function(){
-    console.log('pong!');
-    setTimeout(function(){      
-      try{
-        ws.ping();
-        console.log('ping!');
-      }
-      catch(e){
-      	console.log(e);
-        ws.close();
-      }
-    }, 1000);
-  });
-  ws.ping();
+    // Implement connecton checker
+    (function() {
+
+
+        //
+        var closeConnection = function(ws) {
+            console.log('Remote Client no pong or ping error!');
+            ws.close();
+        };
+
+
+        //
+        var recvPong = true;
+        var pingFunc;
+
+
+        //
+        ws.on('pong', function() {
+            recvPong = true;
+            console.log('pong!');
+        });
+
+
+        //
+        setTimeout(pingFunc = function() {
+            if (recvPong) {
+                try {
+                    ws.ping();
+                    console.log('ping!');
+                    setTimeout(pingFunc, 1000);
+                } catch (e) {
+                    closeConnection(ws);
+                }
+            } else {
+                closeConnection(ws);
+            }
+        }, 1000);
+
+    })();
 
 });
